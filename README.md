1a229a6f37323e6388a83b216743f5845e363814
Matthias Larsen <mlarsen@gammamedia.org>
2019-08-15 15:37:42 +0200

    Named arguments. json config file. external script check. dry run support.

--- README.md
+++ README.md
@@ -1,56 +1,104 @@
 # Killscript
-Script that makes sure to validate the configuration you set up. Set up your own validation rules and run scripts if validation is successful or not.
+Script that makes sure to validate the configuration you set up.
+Set up your own validation rules and run scripts if validation is successful or not.
 
 `killscript.py` is the main file to be run.
 
-The script takes 3 argument (must be written in order):
- 1. argument being the check-config path
- 2. argument being the success script to be run
- 3. argument being the failure script to be run
+```
+usage: killscript.py [-h] [--config CONFIG] [--dry-run]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --config CONFIG, -c CONFIG
+                        Path to config file.
+  --dry-run, -d         Execute as dry run. Print check results and commands
+                        rather than executing commands.
+```
  
 Example 
 ```
-python killscript.py "checkConfig.txt" "echo Success" "echo Failure"
+python killscript.py -c config.json
 ```
 
-## Check-config
-Check config is the run configuration for the script.
-
-The way it works is that the file is read line by line (if a line is empty it is ignored).
-
-Each line contains an action, value and an optional extra value. Action, value and the extra value are all separated with a colon (:).
-
-An action can be one of the following:
- - minchecks - the minimum checks that needs to be validated true
- - ip - the local ip
- - file - generate file hash from a file
+## Config
+The config file is a simple json file with an object containing 4 keys; `minchecks`, `checks`, `success` and `failure`.
  
 ### minchecks 
 minchecks only takes one value which is the number of minimum checks to be successfully.
-
-If multiple minchecks actions are inputted only the last one will take effect.
 
 Example
 ```
 minchecks:1
 ```
 
-### ip
-ip only takes one value being the expected ip (ipv4).
+### checks
+`checks` is an array of strings, each consisting of a handler, value and an optional extra values.
+Handler and the arguments for the handler are separated by a colon (:).
+Arguments for the handler are seperated by semicolon (;)
+
+A handler can be one of the following:
+ - ip - the local ip
+ - hash - generate file hash from a file
+ - script - execute a custom external script
+
+#### ip
+ip only takes one value being the expected ip (ipv4). * is supported as wildcard.
 
 Example
 ```
+ip:192.168.*
+ip:192.168.0.*
 ip:192.168.0.10
 ```
 
-### file
-file takes one value and one extra value.
+#### hash
+hash takes two values and an optional extra values.
 
-The value being the file path. Final destination must be a file.
+The first value is the file path.
 
-The extra value being the expected sha256 checksum of the file.
+The last value is the expected checksum of the file.
+
+An optional 2nd value can be provided to set a hash algorithm. Defaults to sha256 if omitted. Any algorithm supported by [hashlib](https://docs.python.org/3/library/hashlib.html#hash-algorithms) is valid.
 
 Example
 ```
-file:./test.txt:4e08b43ed4cb5250e5c503bb876e5180fd0dcfe1581ae488c374aa1e328e0b63
+file:./test.txt;md5;c946bdbebe795dad3ce5a17a6b3afc88
+file:./test.txt;sha256;4e08b43ed4cb5250e5c503bb876e5180fd0dcfe1581ae488c374aa1e328e0b63
+file:./test.txt;4e08b43ed4cb5250e5c503bb876e5180fd0dcfe1581ae488c374aa1e328e0b63
+```
+
+#### script
+script only takes a single argument, the script to run.
+
+The script **must** return exit code 0 for the check to pass.
+
+Example
+```
+script:./custom-check.sh
+```
+
+### success
+A string or array of strings consisting of commands to execute if enough checks pass to satisfy `minchecks`.
+
+### failure
+A string or array of strings consisting of commands to execute if too few checks pass to satisfy `minchecks`.
+
+### Example config
+```
+{
+    "minchecks": 2,
+    "checks": [
+        "ip:192.168.1.10",
+        "ip:192.168.1.*",
+        "ip:192.*.1.10",
+        "hash:./test.txt;sha256;4e08b43ed4cb5250e5c503bb876e5180fd0dcfe1581ae488c374aa1e328e0b63",
+        "hash:./test.txt;md5;4e08b43ed4cb5250e5c503bb876",
+        "script:./custom-check.sh"
+    ],
+    "success": "./start-success.sh",
+    "failure": [
+        "./start-failure.sh",
+        "shutdown -t now"
+    ]
+}
 ```
--- killscript.py
+++ killscript.py
@@ -1,7 +1,16 @@
 import socket
 import hashlib
-import sys
+import argparse, sys
 import os
+import json
+import re
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--config', '-c', help='Path to config file.', type= str)
+parser.add_argument('--dry-run', '-d', help='Execute as dry run. Print check results and commands rather than executing commands.', action='store_true')
+
+args=parser.parse_args()
+DRY_RUN = args.dry_run
 
 def getIp():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,61 +26,121 @@ def getIp():
 
     return result
 
-def getFileHash(path):
-    sha256_hash = hashlib.sha256()
-    with open(path, "rb") as f:
-        for byte_block in iter(lambda: f.read(4096),b""):
-            sha256_hash.update(byte_block)
+def checkIp(input):
+    if DRY_RUN:
+        print("Running IP check")
 
-        return sha256_hash.hexdigest()
+    curr = getIp()
+    exp = input.replace(".", "\.").replace("*", ".*?")
+    isMatch = re.search("^{0}$".format(exp), curr) is not None
 
-def check(path):
-    minChecks = 0
+    if DRY_RUN:
+        print("- Got {0}, expected {1} => {2}".format(curr, input, isMatch))
+
+    return isMatch
+
+def getFileHash(path, hashfn):
+    allowed = hashlib.algorithms
+    if hashfn in allowed:
+        fun = getattr(hashlib, hashfn)()
+        with open(path, "rb") as f:
+            for byte_block in iter(lambda: f.read(4096),b""):
+                fun.update(byte_block)
+
+            return fun.hexdigest()
+    else:
+        raise NameError('Not a valid algorithm: ' + hashfn)
+
+def checkFileHash(input):
+    file, expected = input.split(";", 1)
+    if ";" in expected:
+        method, expected = expected.split(";", 1)
+    else:
+        method = "sha256"
+    
+    if DRY_RUN:
+        print("Calculating {0} of '{1}'".format(method, file))
+    
+    hash = getFileHash(file, method)
+
+    if DRY_RUN:
+        print("- Got {0}, expected {1} => {2}".format(hash, expected, hash == expected))
+
+    return hash == expected
+
+def executeScript(script):
+    if DRY_RUN:
+        print("Executing external check script: {0}".format(script))
+
+    retVal = os.system(script)
+
+    if DRY_RUN:
+        print("- Got return value {0}".format(retVal))
+    return retVal == 0
+
+def runChecks(checks, handlers):
+    global DRY_RUN
     checkSuccessCount = 0
+    checksRun = 0
 
-    with open(path, "r") as f:
-        for line in f:
-            try:
-                line = line.strip()
-                if not line:
-                    continue
-
-                split = line.split(":")
-                action = split[0].lower()
-                if (action == "minchecks"):
-                    try:
-                        minChecks = int(split[1])
-                    except:
-                        minChecks = 0
-
-                elif (action == "ip"):
-                    currentIp = getIp().lower()
-                    expectedIp = split[1].lower()
-                    if (currentIp == expectedIp):
-                        checkSuccessCount += 1
-
-                elif (action == "file"):
-                    currentFileHash = getFileHash(split[1]).lower()
-                    expectedFileHash = split[2].lower()
-                    if (currentFileHash == expectedFileHash):
-                        checkSuccessCount += 1
-            except:
+    for check in checks:
+        try:
+            check = check.strip()
+            if not check:
                 continue
 
-    return minChecks > 0 and checkSuccessCount >= minChecks
+            action, args = check.split(":", 1)
 
-checkPath = sys.argv[1]
+            if action.lower() in handlers:
+                if handlers[action.lower()](args):
+                    checkSuccessCount += 1
+                checksRun += 1
+            elif DRY_RUN:
+                print("!! UNKNOWN CHECK: {0}".format(action.lower()))
+        except Exception as e:
+            if DRY_RUN:
+                print("EXCEPTION:")
+                print(e)
+            continue
 
-checkStatus = False
+    return checkSuccessCount, checksRun
+
+handlerMap = {
+    "ip": checkIp,
+    "hash": checkFileHash,
+    "script": executeScript
+}
+
 try:
-    checkStatus = check(checkPath)
-except:
-    checkStatus = False
+    with open(args.config, 'r') as f:
+        conf = json.load(f)
+        if DRY_RUN:
+            print("Config:")
+            print(json.dumps(conf, indent=4, sort_keys=True))
 
+        passed, run = runChecks(conf["checks"], handlerMap)
+        success = conf["minchecks"] > 0 and passed >= conf["minchecks"]
 
-if checkStatus:
-    mountCommand = sys.argv[2]
-    os.system(mountCommand)
-else:
-    failureCommand = sys.argv[3]
-    os.system(failureCommand)
+        if DRY_RUN:
+            print("======")
+            print("Checks run: {0}".format(run))
+            print("Checks passed: {0}".format(passed))
+            print("Check required: {0}".format(conf["minchecks"]))
+            print("Success: {0}".format(success))
+            print("======")
+        
+        resultingCommands = conf["success"] if success else conf["failure"]
+        PY3 = sys.version_info[0] == 3
+        string_types = str if PY3 else basestring
+        if isinstance(resultingCommands, string_types):
+            resultingCommands = [resultingCommands]
+
+        for cmd in resultingCommands:
+            if DRY_RUN:
+                print("EXEC: {0}".format(cmd))
+            else:
+                os.system(cmd)
+except Exception as e:
+    if DRY_RUN:
+        print("Exception")
+        print(e)
